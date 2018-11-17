d558e48e859fc2b7dd87810af4056f19fe5c4ebe
cgarwood <cgarwood@gmail.com>
2018-11-17 14:45:27 -0500

    Starting Stage Display API Support

--- server/components/propresenter.js
+++ server/components/propresenter.js
@@ -5,9 +5,65 @@
     Register functions/methods for ProPresenter control
 */
 
+const WebSocket = require('ws');
+
+const CONFIG = require('../config.js');
 const logger = require('../logger.js');
 
+class StageDisplayApi {
+    constructor() {
+        this.host = CONFIG.components.propresenter.host;
+        this.port = CONFIG.components.propresenter.port;
+        this.stage_display_password =
+            CONFIG.components.propresenter.stage_display_password;
+        this.connected = false;
+
+        logger.debug(
+            `Connecting to Stage Display API at ${this.host}:${this.port}`
+        );
+
+        this.ws = new WebSocket(`ws://${this.host}:${this.port}/stagedisplay`);
+        this.ws.on('open', () => {
+            this.ws.send(
+                `{"pwd":"${this.stage_display_password}","ptl":610,"acn":"ath"}`
+            );
+            logger.debug('Stage Display API connected');
+        });
+        this.ws.on('message', data => {
+            this.handleMessage(data);
+        });
+        this.ws.on('close', () => {
+            logger.error('ProPresenter Stage Display API disconnected');
+        });
+    }
+
+    handleMessage(data) {
+        this.data = JSON.parse(data);
+        switch (this.data.acn) {
+            case 'ath':
+                if (this.data.ath === false) {
+                    logger.error(
+                        `ProPresenter Stage Display Authentication Failed: ${
+                            this.data.err
+                        }`
+                    );
+                } else {
+                    this.connected = true;
+                    logger.debug(
+                        'Stage Display API Connected and Authenticated'
+                    );
+                }
+                break;
+            default:
+                break;
+        }
+        logger.debug(`stage display websocket data: ${data}`);
+    }
+}
+
 async function setupComponent() {
+    this.stageDisplayApi = new StageDisplayApi();
+
     logger.info('ProPresenter component initialized');
 }
 module.exports = { setupComponent };
