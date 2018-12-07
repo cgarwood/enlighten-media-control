3c92cc18f1065c4a280b264a0c75ba64a88ca49e
cgarwood <cgarwood@gmail.com>
2018-12-07 15:57:37 -0500

    More work on Onyx component

--- server/components/onyx.js
+++ server/components/onyx.js
@@ -24,8 +24,21 @@ class OnyxController extends EventEmitter {
         super();
         this.host = CONFIG.components.onyx.host;
         this.port = CONFIG.components.onyx.port;
+        this.pollingInterval = CONFIG.components.onyx.polling_interval;
 
         this.connected = false;
+        this.onyxRunning = false;
+
+        this.activeCuelists = [];
+
+        this.telnetListener = new Telnet();
+        this.telnetSettings = {
+            host: this.host,
+            port: this.port,
+            shellPrompt: '',
+            timeout: 1500,
+            negotiationMandatory: false,
+        };
     }
 
     emit(event, ...data) {
@@ -37,13 +50,84 @@ class OnyxController extends EventEmitter {
         logger.debug(
             `Connecting to Onyx Telnet Interface at ${this.host}:${this.port}`
         );
-        const telnetListener = new Telnet();
-        await telnetListener.connect({
-            host: this.host,
-            port: this.port,
-            shellPrompt: '',
-            timeout: 1500,
-            negotiationMandatory: false,
+
+        this.telnetListener.on('close', () => {
+            logger.debug(`Onyx Telnet Connection Closed`);
+        });
+
+        await this.telnetListener.connect(this.telnetSettings);
+        this.heartbeat();
+    }
+
+    async heartbeat() {
+        // Check if Onyx is running
+        const res = await this.telnetListner.send('IsMXRun', {
+            waitfor: '.\r\n',
+        });
+        const lines = res.split('\r\n');
+        lines.forEach(line => {
+            switch (line) {
+                case 'Yes':
+                    this.onyxRunning = true;
+                    break;
+                case 'No':
+                    this.onyxRunning = false;
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        // Check for active cuelists
+        await this.getActiveCuelists();
+
+        // Loop
+        setTimeout(() => {
+            this.heartbeat();
+        }, this.pollingInterval);
+    }
+
+    async runTelnet(command) {
+        // Initiate a new Telnet connection
+        const telnetProcessor = new Telnet();
+        await telnetProcessor.connect(this.telnetSettings);
+
+        // Send the command and wait for the result
+        const telnetResult = await telnetProcessor.send(command, {
+            waitfor: '.\r\n',
+        });
+
+        // After receiving result, disconnect
+        telnetProcessor.end();
+
+        // Parse and return the result
+        const lines = telnetResult.split('\r\n');
+
+        // Remove first 2 welcome lines
+        delete lines[0];
+        delete lines[1];
+
+        return lines;
+    }
+
+    async getActiveCuelists() {
+        const res = await this.runTelnet('QLActive');
+        this.activeCuelists = [];
+        res.forEach(line => {
+            switch (line) {
+                case '200 Ok':
+                case '200 ':
+                case '.':
+                case '':
+                case 'No Active Qlist in List':
+                    break;
+                default: {
+                    const id = parseInt(line.substring(0, 5), 10);
+                    const title = line.substring(8);
+                    this.activeCuelists.push({ id, title });
+                    break;
+                }
+            }
         });
     }
 }
